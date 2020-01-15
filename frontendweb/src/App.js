3a8db7438919e910a9ca56fbd8b464a854171545
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-01-15 13:41:02 -0300

    aside finalizado.

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import './Global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
-    <div id='#app'>
+    <div id='app'>
       <aside>
+        <h1>Cadastrar</h1>
+        <form>
+          <div className='input-block'>
+            <label htmlFor='github_username'>Usuário do Github</label>
+            <input
+              type='text'
+              name='github_username'
+              id='github_username'
+              required
+            />
+          </div>
 
+          <div className='input-block'>
+            <label htmlFor='techs'>Tecnologias</label>
+            <input type='text' name='techs' id='techs' />
+          </div>
+
+          <div className='input-group'>
+            <div className='input-block'>
+              <label htmlFor='latitude'>Latitude</label>
+              <input type='text' name='latitude' id='latitude' />
+            </div>
+
+            <div className='input-block'>
+              <label htmlFor='longitude'>Longitude</label>
+              <input type='text' name='longitude' id='longitude' />
+            </div>
+          </div>
+
+          <button type='submit'>Salvar</button>
+        </form>
       </aside>
-      <main>
-        
-      </main>
+      <main></main>
     </div>
   );
 }
