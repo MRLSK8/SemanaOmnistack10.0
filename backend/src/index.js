1d370120e993b6eba5ffaf6d00c695d30601966f
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-01-19 19:28:27 -0300

    ...

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,13 @@ const PORT = process.env.PORT || 3333;
 const mongoose = require('mongoose');
 const { url, flags } = require('../config/database');
 const routes = require('./routes');
+const cors = require('cors');
 
 mongoose.connect(url, flags);
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,54 +1,99 @@
-import React from "react";
-import "./App.css";
-import "./Global.css";
-import "./Sidebar.css";
-import "./Main.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import './Global.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithub_username] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      possition => {
+        const { latitude, longitude } = possition.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  const handelSubmit = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div id="app">
+    <div id='app'>
       <aside>
         <h1>Cadastrar</h1>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
+        <form onSubmit={handelSubmit}>
+          <div className='input-block'>
+            <label htmlFor='github_username'>Usuário do Github</label>
             <input
-              type="text"
-              name="github_username"
-              id="github_username"
+              type='text'
+              name='github_username'
+              id='github_username'
               required
+              value={github_username}
+              onChange={event => setGithub_username(event.target.value)}
             />
           </div>
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input type="text" name="techs" id="techs" />
+          <div className='input-block'>
+            <label htmlFor='techs'>Tecnologias</label>
+            <input
+              type='text'
+              name='techs'
+              id='techs'
+              value={techs}
+              onChange={event => setTechs(event.target.value)}
+            />
           </div>
 
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="text" name="latitude" id="latitude" />
+          <div className='input-group'>
+            <div className='input-block'>
+              <label htmlFor='latitude'>Latitude</label>
+              <input
+                type='number'
+                value={latitude}
+                name='latitude'
+                id='latitude'
+                onChange={event => setLatitude(event.target.value)}
+              />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="text" name="longitude" id="longitude" />
+            <div className='input-block'>
+              <label htmlFor='longitude'>Longitude</label>
+              <input
+                type='number'
+                value={longitude}
+                name='longitude'
+                id='longitude'
+                onChange={event => setLongitude(event.target.value)}
+              />
             </div>
           </div>
 
-          <button type="submit">Salvar</button>
+          <button type='submit'>Salvar</button>
         </form>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
+          <li className='dev-item'>
             <header>
               <img
-                src="https://avatars1.githubusercontent.com/u/39857752?s=460&v=4"
-                alt="Marcelo Lima"
+                src='https://avatars1.githubusercontent.com/u/39857752?s=460&v=4'
+                alt='Marcelo Lima'
               />
-              <div className="user-info">
+              <div className='user-info'>
                 <h1>Marcelo Lima</h1>
                 <span>ReactJS, React Native, Node.js</span>
               </div>
@@ -58,21 +103,21 @@ function App() {
               Pernambuco (UPE). Currently in the 5th period!
             </p>
             <a
-              href="https://github.com/MRLSK8"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://github.com/MRLSK8'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               Acessar perfil no github
             </a>
           </li>
 
-          <li className="dev-item">
+          <li className='dev-item'>
             <header>
               <img
-                src="https://avatars1.githubusercontent.com/u/39857752?s=460&v=4"
-                alt="Marcelo Lima"
+                src='https://avatars1.githubusercontent.com/u/39857752?s=460&v=4'
+                alt='Marcelo Lima'
               />
-              <div className="user-info">
+              <div className='user-info'>
                 <h1>Marcelo Lima</h1>
                 <span>ReactJS, React Native, Node.js</span>
               </div>
@@ -82,21 +127,21 @@ function App() {
               Pernambuco (UPE). Currently in the 5th period!
             </p>
             <a
-              href="https://github.com/MRLSK8"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://github.com/MRLSK8'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               Acessar perfil no github
             </a>
           </li>
 
-          <li className="dev-item">
+          <li className='dev-item'>
             <header>
               <img
-                src="https://avatars1.githubusercontent.com/u/39857752?s=460&v=4"
-                alt="Marcelo Lima"
+                src='https://avatars1.githubusercontent.com/u/39857752?s=460&v=4'
+                alt='Marcelo Lima'
               />
-              <div className="user-info">
+              <div className='user-info'>
                 <h1>Marcelo Lima</h1>
                 <span>ReactJS, React Native, Node.js</span>
               </div>
@@ -106,21 +151,21 @@ function App() {
               Pernambuco (UPE). Currently in the 5th period!
             </p>
             <a
-              href="https://github.com/MRLSK8"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://github.com/MRLSK8'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               Acessar perfil no github
             </a>
           </li>
 
-          <li className="dev-item">
+          <li className='dev-item'>
             <header>
               <img
-                src="https://avatars1.githubusercontent.com/u/39857752?s=460&v=4"
-                alt="Marcelo Lima"
+                src='https://avatars1.githubusercontent.com/u/39857752?s=460&v=4'
+                alt='Marcelo Lima'
               />
-              <div className="user-info">
+              <div className='user-info'>
                 <h1>Marcelo Lima</h1>
                 <span>ReactJS, React Native, Node.js</span>
               </div>
@@ -130,9 +175,9 @@ function App() {
               Pernambuco (UPE). Currently in the 5th period!
             </p>
             <a
-              href="https://github.com/MRLSK8"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://github.com/MRLSK8'
+              rel='noopener noreferrer'
+              target='_blank'
             >
               Acessar perfil no github
             </a>
