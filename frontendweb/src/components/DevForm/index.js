758ad95028f5a9c1945c99b05a996c843ea25328
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-01-20 01:16:31 -0300

    Frontend web finalizado.

--- frontendweb/src/components/DevForm/index.js
+++ frontendweb/src/components/DevForm/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import './styles.css';
-
 export default function DevForm({ onSubmit }) {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
